01e77e4e9590b6172abba381bbc4d56d2a518c3d
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2025-05-19 18:41:08 -0300

    primer versión

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+"""Programa que permita realizar las operaciones basicas matemáticas"""
+
+def sumar(num1, num2):
+    resultado=num1 + num2 
+    return resultado
+
+def restar(num1, num2):
+    resultado=num1-num2
+    return resultado
---
+++ main.py
@@ -0,0 +1,18 @@
+from calculadora import sumar_dos_numeros, restar_dos_numeros
+
+#Programa Principal (interfaz gráfica - print, input)
+while True:
+    opcion=input("Ingrese S para sumar, R para restar, M para multiplicar y X para Salir").lower()
+
+    if opcion=="x":
+        break
+    elif opcion=="s":
+        n1=int(input("Ingrese el número 1: "))
+        n2=int(input("Ingrese el número 2: "))
+        print(f"El resultado de la suma es : {sumar_dos_numeros(n1,n2)}")
+    elif opcion=="r":
+        n1=int(input("Ingrese el número 1: "))
+        n2=int(input("Ingrese el número 2: "))
+        print(f"El resultado de la resta es : {restar_dos_numeros(n1,n2)}")
+    else:
+        print("El valor ingresado no es válido")
