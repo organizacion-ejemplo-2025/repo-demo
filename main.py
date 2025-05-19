723d30a75cdbb96a3555a71cd5affdb4a7c3b7ba
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2025-05-19 18:49:15 -0300

    se modifico el mensaje

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ while True:
         n2=int(input("Ingrese el número 2: "))
         print(f"El resultado de la resta es : {restar_dos_numeros(n1,n2)}")
     else:
-        print("El valor ingresado no es válido")
+        print("El valor ingresado no es válido. Por favor intente nuevamente.")
